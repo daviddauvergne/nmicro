182a6b3da3585f107338b91cfcfba0ae4e1c544d
David Dauvergne <david.dauvergne@archicol.fr>
2017-11-13 20:47:27 +0100

    fixed add thme

--- demo/sources/setting.js
+++ demo/sources/setting.js
@@ -1,37 +1,37 @@
 
 module.exports = {
-	default : {// theme
-		app : {// out app
-			dev : {// mode dev
-				NMICRO_WEB_SERVER_URL : 'http://localhost:8080/',
-				languesName: {
-					'fr': 'français',
-					'en': 'english'
-				}
-			},
-			prod : {// mode prod
-				NMICRO_WEB_SERVER_URL : 'http://www.domain.com/',
-				languesName: {
-					'fr': 'français',
-					'en': 'english'
-				}
-			}
-		},
-		web : {// out web
-			dev : {// mode dev
-				NMICRO_WEB_SERVER_URL : 'http://localhost:8080/',
-				languesName: {
-					'fr': 'français',
-					'en': 'english'
-				}
-			},
-			prod : {// mode prod
-				NMICRO_WEB_SERVER_URL : 'http://www.domain.com/',
-				languesName: {
-					'fr': 'français',
-					'en': 'english'
-				}
-			}
-		}
-	}
+ "default": {
+  "app": {
+   "dev": {
+    "NMICRO_WEB_SERVER_URL": "http://localhost:8080/",
+    "languesName": {
+     "fr": "français",
+     "en": "english"
+    }
+   },
+   "prod": {
+    "NMICRO_WEB_SERVER_URL": "http://www.domain.com/",
+    "languesName": {
+     "fr": "français",
+     "en": "english"
+    }
+   }
+  },
+  "web": {
+   "dev": {
+    "NMICRO_WEB_SERVER_URL": "http://localhost:8080/",
+    "languesName": {
+     "fr": "français",
+     "en": "english"
+    }
+   },
+   "prod": {
+    "NMICRO_WEB_SERVER_URL": "http://www.domain.com/",
+    "languesName": {
+     "fr": "français",
+     "en": "english"
+    }
+   }
+  }
+ }
 };
